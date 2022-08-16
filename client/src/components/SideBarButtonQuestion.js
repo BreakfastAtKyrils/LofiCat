9d9a660619468aca364410fd77e1e0fa006af540
Nika Ptushkina <nika.ptushkina@gmail.com>
2022-08-15 21:08:57 -0700

    Add checkmarks or Xs for right and wrong answer - HTML and CSS only

--- client/src/components/SideBarButtonQuestion.js
+++ client/src/components/SideBarButtonQuestion.js
@@ -3,10 +3,18 @@ import React from "react";
 const SideBarButtonQuestion = (props) => {
   return (
     <>
-      <button type="button" class="overlap-group1" onClick={props.onClick}>
-        <div class="question valign-text-middle poppins-normal-black-25px">
-          {props.questionNumber}
-        </div>
+        <button type="button" class="overlap-group-container-1" onClick={props.onClick}>
+              <div class="overlap-group-5">
+                <div class="question valign-text-middle poppins-normal-black-25px">{props.questionNumber}</div>
+              </div>
+              <div class="overlap-group1-3">
+                <img class="ellipse-3" src="img/ellipse-3-9@2x.svg" />
+                <div class="a valign-text-middle poppins-normal-white-30px">✔️</div>
+              </div>
+              {/* <div class="overlap-group1-3">
+                <img class="ellipse-3" src="img/ellipse-3-10@2x.svg" />
+                <div class="a valign-text-middle poppins-normal-white-30px">❌</div>
+              </div> */}
       </button>
     </>
   );
